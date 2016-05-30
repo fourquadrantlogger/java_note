3198c90fb3ddb5fbef393a5227072f6011e5dda0
timeloveboy <734991033@qq.com>
2016-05-30 22:57:41 +0800

    ~

---
+++ src/compute_exp/OpType.java
@@ -0,0 +1,9 @@
+package compute_exp;
+
+/**
+ * Created by timeloveboy on 16/5/30.
+ */
+public class OpType {
+    public char[] data;//运算符
+    public int top;//栈顶指针
+}
---
+++ src/compute_exp/ValueType.java
@@ -0,0 +1,9 @@
+package compute_exp;
+
+/**
+ * Created by timeloveboy on 16/5/30.
+ */
+public class ValueType {
+    public double[] data;//存放云算数
+    public int top;//栈顶指针
+}
